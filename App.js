5cbab12c2fb586d944bd8753daec6fa56e875991
Raghavkhandelwal7 <71634967+Raghavkhandelwal7@users.noreply.github.com>
2021-04-16 10:43:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer, NavigationContext} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs'
+
+export default class App extends React.Component {
+  return (
+    
+  );
+}
+
+
